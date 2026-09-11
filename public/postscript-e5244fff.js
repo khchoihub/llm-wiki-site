955b1534944a0e26c7ee6206b0b58a50e6fd8515
khchoihub <choikihun@gmail.com>
2026-09-11 10:51:11 +0900

    sync: 사이트 탐색기와 목차 복구

--- public/postscript-e5244fff.js
+++ public/postscript-e5244fff.js
@@ -6,11 +6,11 @@ await Promise.all([
   import("./static/scripts/script-4-ec880d15.js"),
   import("./static/scripts/script-5-7c280710.js"),
   import("./static/scripts/script-6-53882546.js"),
-  import("./static/scripts/script-7-5ef560a8.js"),
-  import("./static/scripts/script-8-632b2822.js"),
-  import("./static/scripts/script-9-1a7d3efa.js"),
-  import("./static/scripts/script-10-cc8afa9d.js"),
-  import("./static/scripts/script-11-7e107d7e.js"),
+  import("./static/scripts/script-7-cc8afa9d.js"),
+  import("./static/scripts/script-8-7e107d7e.js"),
+  import("./static/scripts/script-9-5ef560a8.js"),
+  import("./static/scripts/script-10-632b2822.js"),
+  import("./static/scripts/script-11-1a7d3efa.js"),
   import("./static/scripts/script-12-5f71ef20.js"),
   import("./static/scripts/script-13-a2f22c29.js")
 ]);
